23e337f5ca44bdec8af1b1863d9755218028e148
GabiBarbera <gabriel.barberaa@gmail.com>
2023-11-21 13:37:22 -0300

    herencia

--- src/Herencia/domain/Empleado.java
+++ src/Herencia/domain/Empleado.java
@@ -5,8 +5,40 @@ public class Empleado extends Persona {
     private double sueldo;
     private static int contadorEmpleado;
 
-    public Empleado(int idEmpleado, double sueldo) {
-        this.idEmpleado = idEmpleado;
+    public Empleado() {
+        this.idEmpleado = ++Empleado.contadorEmpleado;
+    }
+
+    public Empleado(String nombre, double sueldo) {
+        // super(nombre);
+        // this.idEmpleado = ++Empleado.contadorEmpleado;
+        this();
+        this.nombre = nombre;
         this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "idEmpleado=" + idEmpleado +
+                ", sueldo=" + sueldo +
+                ", " + super.toString() + "}" +
+                '}';
     }
 }
--- src/Herencia/test/TestHerencia.java
+++ src/Herencia/test/TestHerencia.java
@@ -1,10 +1,20 @@
 package Herencia.test;
 
+import Herencia.domain.Cliente;
 import Herencia.domain.Empleado;
+import Herencia.domain.Persona;
+
+import java.util.Date;
 
 public class TestHerencia {
     public static void main(String[] args) {
-        Empleado empleado = new Empleado();
+        Empleado empleado = new Empleado("Juan",5000.0);
         System.out.println("empleado = " + empleado);
+//        Date fecha = new Date();
+//
+//        Cliente cliente = new Cliente(fecha,true,"Karla",'F',28,"Saturno 15");
+//        System.out.println("cliente = " + cliente);
+//
+//        Persona persona = new Persona();
     }
 }
