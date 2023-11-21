3c2df0d15877e91837d2289efe881ce63ebe76e5
GabiBarbera <gabriel.barberaa@gmail.com>
2023-11-21 14:57:28 -0300

    final de fainal

--- src/PalabraFinal/test/TestFinal.java
+++ src/PalabraFinal/test/TestFinal.java
@@ -9,8 +9,16 @@ public class TestFinal {
 
         // miVariable = 5; No se puede modificar
 
-      //   Persona.MI_CONSTANTE = 5; no se puede modificar
+        //   Persona.MI_CONSTANTE = 5; no se puede modificar
 
         System.out.println("Mi constante = " + Persona.MI_CONSTANTE);
+
+        final Persona persona1 = new Persona();
+        // persona1 = new Persona();
+        System.out.println("persona1 = " + persona1.getNombre());
+        persona1.setNombre("Pedro");
+        System.out.println("Persona 1 nombre = " + persona1.getNombre());
+        persona1.setNombre("Carlos");
+        System.out.println("persona1 nombre = " + persona1.getNombre());
     }
 }
