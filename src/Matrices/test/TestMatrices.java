55697820e084690c8a7446bf832b04afab1a9a5d
GabiBarbera <gabriel.barberaa@gmail.com>
2023-11-22 16:45:04 -0300

    matricez

--- src/Matrices/test/TestMatrices.java
+++ src/Matrices/test/TestMatrices.java
@@ -1,5 +1,8 @@
 package Matrices.test;
 
+
+import Matrices.domain.Persona;
+
 public class TestMatrices {
     public static void main(String[] args) {
         int edades[][] = new int[3][2];
@@ -18,17 +21,31 @@ public class TestMatrices {
 //        System.out.println("edades 1-1 = " + edades[1][1]);
 //        System.out.println("edades 2-0 = " + edades[2][0]);
 //        System.out.println("edades 2-1 = " + edades[2][1]);
+//
+//        for (int ren = 0; ren < edades.length; ren++) {
+//            for (int col = 0; col < edades[ren].length; col++) {
+//                System.out.println("Edades " + ren + "-" + col + ": " + edades[ren][col]);
+//            }
+//        }
 
-        for (int ren = 0; ren < edades.length; ren++) {
-            for (int col = 0; col < edades[ren].length; col++) {
-                System.out.println("Edades " + ren + "-" + col + ": " + edades[ren][col]);
-            }
-        }
+        String frutas[][] = {{"Naranja", "Limon"}, {"Frutilla", "Banana", "Mora"}, {"Melon", "Sandia"}};
+        imprimir(frutas);
 
-        String frutas[][] = {{"Naranja", "Limon"}, {"Frutilla", "Banana","Mora"},{"Melon","Sandia"}};
-        for (int ren = 0; ren < frutas.length; ren++) {
-            for (int col = 0; col < frutas[ren].length; col++) {
-                System.out.println("Frutas = " + ren + "-" + col + ": " + frutas[ren][col]);
+        Persona personas[][] = new Persona[2][3];
+        personas[0][0] = new Persona("Juan");
+        personas[0][1] = new Persona("Karla");
+        personas[0][2] = new Persona("Pedro");
+        personas[1][0] = new Persona("Juana");
+        personas[1][1] = new Persona("Maria");
+        personas[1][2] = new Persona("Ricardo");
+        imprimir(personas);
+
+    }
+
+    public static void imprimir(Object matriz[][]) {
+        for (int ren = 0; ren < matriz.length; ren++) {
+            for (int col = 0; col < matriz[ren].length; col++) {
+                System.out.println("Matriz = " + ren + "-" + col + ": " + matriz[ren][col]);
             }
         }
     }
