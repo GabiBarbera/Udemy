437ea44fce427cb014dd9e09124f59c1988a1c96
GabiBarbera <gabriel.barberaa@gmail.com>
2023-11-22 13:35:02 -0300

    matrices

---
+++ src/Matrices/test/TestMatrices.java
@@ -0,0 +1,35 @@
+package Matrices.test;
+
+public class TestMatrices {
+    public static void main(String[] args) {
+        int edades[][] = new int[3][2];
+//        System.out.println("edades = " + edades);
+
+        edades[0][0] = 5;
+        edades[0][1] = 7;
+        edades[1][0] = 8;
+        edades[1][1] = 4;
+        edades[2][0] = 1;
+        edades[2][1] = 10;
+
+//        System.out.println("edades 0-0 = " + edades[0][0]);
+//        System.out.println("edades 0-1 = " + edades[0][1]);
+//        System.out.println("edades 1-0 = " + edades[1][0]);
+//        System.out.println("edades 1-1 = " + edades[1][1]);
+//        System.out.println("edades 2-0 = " + edades[2][0]);
+//        System.out.println("edades 2-1 = " + edades[2][1]);
+
+        for (int ren = 0; ren < edades.length; ren++) {
+            for (int col = 0; col < edades[ren].length; col++) {
+                System.out.println("Edades " + ren + "-" + col + ": " + edades[ren][col]);
+            }
+        }
+
+        String frutas[][] = {{"Naranja", "Limon"}, {"Frutilla", "Banana","Mora"},{"Melon","Sandia"}};
+        for (int ren = 0; ren < frutas.length; ren++) {
+            for (int col = 0; col < frutas[ren].length; col++) {
+                System.out.println("Frutas = " + ren + "-" + col + ": " + frutas[ren][col]);
+            }
+        }
+    }
+}
