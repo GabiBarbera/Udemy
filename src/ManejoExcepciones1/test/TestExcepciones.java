6dac235b08627354acdc35c19d0e0db2e6953484
GabiBarbera <gabriel.barberaa@gmail.com>
2023-12-13 15:39:48 -0300

    Manejo de Excepciones 2

--- src/ManejoExcepciones1/test/TestExcepciones.java
+++ src/ManejoExcepciones1/test/TestExcepciones.java
@@ -1,13 +1,24 @@
 package ManejoExcepciones1.test;
 
+import ManejoExcepciones1.aritmetica.Aritmetica;
+import ManejoExcepciones1.excepciones.OperacionExcepcion;
+
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
         try {
-            resultado = 10 / 0;
-        }catch (Exception e) {
-            System.out.println("Ocurrio un error: ");
+            resultado = Aritmetica.division(10, 0);
+        }catch (OperacionExcepcion e){
+            System.out.println("Ocurrio un error de tipo OperacionExcepcion: ");
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println("Ocurrio un error de tipo Excepcion: ");
             e.printStackTrace(System.out);
+            System.out.println(e.getMessage());;
+        }
+        finally {
+            System.out.println("Se realizo la division entre cero");
         }
         System.out.println("resultado = " + resultado);
     }
